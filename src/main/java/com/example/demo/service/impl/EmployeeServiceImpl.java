3598c7b96ad3057e48da8b04e11d87f700ad75df
cidealwis <chathunishara4@gmail.com>
2024-10-20 21:37:58 +0530

    first commit

---
+++ src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.dto.EmployeeDto;
+import com.example.demo.entity.Employee;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.mapper.EmployeeMapper;
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        Employee employee= EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee=employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+       Employee employee= employeeRepository.findById(employeeId)
+                .orElseThrow(()->
+               new ResourceNotFoundException("employee is not exists"+employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+}
